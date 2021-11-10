15041a389ae28971a91253743afad9ea1d7cafc8
dimaTsaruk <dima.beseder@gmail.com>
2021-11-09 21:44:55 -0600

    two more add

---
+++ src/codingBat/altPairs.java
@@ -0,0 +1,47 @@
+package codingBat;
+
+public class altPairs {
+    /*
+    Given a string, return a string made of
+    the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+    altPairs("kitten") → "kien"
+    altPairs("Chocolate") → "Chole"
+    altPairs("CodingHorror") → "Congrr"
+     */
+
+
+    public static void main(String[] args) {
+        System.out.println(altPairs1("kitten"));
+        System.out.println(altPairs2("kitten"));
+    }
+
+    public static String altPairs1(String str) {
+        String result = "";
+
+        // Run i by 4 to hit 0, 4, 8, ...
+        for (int i = 0; i < str.length(); i += 4) {
+            // Append the chars between i and i+2
+            int end = i + 2;
+            if (end > str.length()) {
+                end = str.length();
+            }
+            result = result + str.substring(i, end);
+        }
+
+        return result;
+    }
+
+    public static String altPairs2(String str) {
+        char[] array = str.toCharArray();
+        String line = "";
+
+        if (str.length() == 1) {
+            return str;
+        } else
+            for (int i = 0; i < array.length - 1; i += 4) {
+                line += String.valueOf("" + array[i] + array[i + 1]);
+            }
+        return line;
+    }
+
+}
---
+++ src/codingBat/array123.java
@@ -0,0 +1,24 @@
+package codingBat;
+
+public class array123 {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 2, 3, 1};
+        System.out.println(array123(array));
+    }
+
+    public static boolean array123(int[] nums) {
+        int[] newArray = new int[nums.length + 2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == 1) {
+                if (nums[i + 1] == 2) {
+                    if (nums[i + 2] == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+}
